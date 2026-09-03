af8d18872bc6eeff07edbb4cc82e63776cfeee6e
Sahana U <sahana.sana547826@gmail.com>
2026-09-03 15:51:18 +0530

    fix: allow new frontend origin

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://expense-tracker-wztl.onrender.com'
+    origin: 'https://expense-tracker-web-yth4.onrender.com'
 }));
 
 const PORT=3000;
